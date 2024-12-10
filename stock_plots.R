f883f09b21fbb99173003e6c4c9d36588c839390
Yash Dalal <50106830+YashDalal10@users.noreply.github.com>
2024-12-10 16:24:37 +0530

    with lollipop plot

--- stock_plots.R
+++ stock_plots.R
@@ -12,3 +12,15 @@ print(valuation)
 treemap(valuation, index = "Scrip", vSize = "Valuation", type = "index",
         fontsize.labels = 9, fontcolor.labels = "black", fontface.labels = 1,
         palette = "Set3", title = "Stock Valuation", fontsize.title = 12)
+
+ggplot(valuation, aes(x=Valuation, y=Scrip)) +
+  geom_segment(aes(x=0, xend = Valuation, y = Scrip, yend = Scrip), color = "skyblue") +
+  geom_point(color = "blue", size = 4, alpha = 0.6) +
+  theme_light() +
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.y = element_blank(),
+    axis.text.y = element_text(size = 9)
+  ) +
+  labs(x="Valuation", y="Scrip", title="Bar Chart of Scrip wise Valuation")
